463b2859a26159917b40e684b1dac18c4000ae7c
Yan Khachko <a@slnk.icu>
2022-10-01 19:16:40 +0300

    by subnet metric added

--- admin_api/main.py
+++ admin_api/main.py
@@ -1,13 +1,79 @@
+from datetime import datetime
+from typing import Union
 from fastapi import FastAPI
+from sqlalchemy.sql import select
+from sqlalchemy import Date, cast, func, text
+import httpagentparser
+import logging
+
+from db_utils import session_scope
+import models
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Yep, another microservice"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/statistics")
+async def statistics(start_datetime: Union[datetime, None],
+                     end_datetime: Union[datetime, None]):
+    async with session_scope() as session:
+        q = select(models.Request.user_agent, models.Request.subnet_uuid) \
+            .where(models.Request.created_at > start_datetime, models.Request.created_at < end_datetime)
+        r = (await session.execute(q)).all()
+        browsers = {}
+        oss = {}
+        for req in r:
+            ua = httpagentparser.detect(req[0])
+            browser_name = ua['browser']['name']
+            os_name = ua['os']['name']
+            if browser_name not in browsers.keys():
+                browsers[browser_name] = 1
+            else:
+                browsers[browser_name] += 1
+            if os_name not in oss.keys():
+                oss[os_name] = 1
+            else:
+                oss[os_name] += 1
+
+        browsers_p = {}
+        os_p = {}
+        for k, v in browsers.items():
+            browsers_p[k] = v / sum(browsers.values())
+        for k, v in oss.items():
+            os_p[k] = v / sum(oss.values())
+
+        q = select(
+            cast(models.Request.created_at, Date).label('date'),
+            func.count().label('count'),
+        ).filter(
+            models.Request.created_at > start_datetime, models.Request.created_at < end_datetime
+        ).group_by(
+            cast(models.Request.created_at, Date),
+        ).order_by(
+            text('date desc')
+        )
+        dates = (await session.execute(q)).all()
+
+        q = select(models.Subnet.name, func.count(models.Request.uuid).label('count'))\
+            .group_by(
+            models.Request.subnet_uuid, models.Subnet.name
+        ).join(
+            models.Subnet.name, models.Subnet.uuid == models.Request.subnet_uuid
+        )
+        subnets = (await session.execute(q)).all()
+
+    return {
+        "dates": dates,
+        "subnets": subnets,
+        "browsers": browsers_p,
+        "os": os_p,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime
+    }
--- admin_api/models.py
+++ admin_api/models.py
@@ -1,44 +1,40 @@
-from sqlalchemy import Column, BigInteger, TIMESTAMP, Text, BOOLEAN, ForeignKey
+from sqlalchemy import Column, BigInteger, TIMESTAMP, Text, BOOLEAN, ForeignKey, text
+from sqlalchemy.dialects.postgresql import INET
 from db_utils import Base as BaseModel
 from db_utils import UUID
 import uuid
 
 
-def generate_uuid():
-    return str(uuid.uuid4())
-
-
-class Tracker(BaseModel):
-    __tablename__ = 'trackers'
-    uuid = Column(UUID, nullable=False, unique=True, primary_key=True)
-    name = Column(Text, nullable=False)
-    owner_id = Column(BigInteger, nullable=False)
-    created_at = Column(TIMESTAMP, nullable=False)
-
-
-# class Request(BaseModel):
-#     __tablename__ = 'requests'
-#     uuid = Column(UUID, nullable=False, unique=True, primary_key=True, default=uuid.uuid4())
-#     ip = Column(Text, nullable=False)
-#     useragent = Column(Text, nullable=True)
-#     from_mask = Column(Text, nullable=False)
-#     mask_owner = Column(Text, nullable=False)
-#     url = Column(Text, nullable=True)
-#     tracker_uuid = Column(UUID, ForeignKey('trackers.uuid'), nullable=False)
-#     at = Column(TIMESTAMP, nullable=False)
-
-
-class Notification(BaseModel):
-    __tablename__ = 'notifications'
-    uuid = Column(UUID, nullable=False, unique=True, primary_key=True, default=generate_uuid())
+class Request(BaseModel):
+    """CREATE TABLE requests (
+        uuid uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+        tracker_uuid uuid NOT NULL REFERENCES trackers (uuid),
+        url text NOT NULL,
+        ip inet NOT NULL,
+        subnet_uuid uuid NOT NULL REFERENCES subnets (uuid),
+        user_agent text NOT NULL,
+        created_at timestamp DEFAULT now() NOT NULL
+    );
+    """
+    __tablename__ = 'requests'
+    uuid = Column(UUID, primary_key=True, server_default=text("uuid_generate_v4()"))
     tracker_uuid = Column(UUID, ForeignKey('trackers.uuid'), nullable=False)
-    chat_id = Column(BigInteger, nullable=False)
-    enable = Column(BOOLEAN, default=True)
+    url = Column(Text, nullable=False)
+    ip = Column(INET, nullable=False)
+    subnet_uuid = Column(UUID, ForeignKey('subnets.uuid'), nullable=False)
+    user_agent = Column(Text, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text("now()"))
 
 
-class Users(BaseModel):
-    __tablename__ = 'users'
-    telegram_id = Column(BigInteger, nullable=False, unique=True, primary_key=True)
-    action = Column(Text)
-    state = Column(Text)
-    created_at = Column(TIMESTAMP, nullable=False)
+class Subnet(BaseModel):
+    """
+        CREATE TABLE subnets (
+            uuid uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+            name text NOT NULL,
+            tag_uuid uuid NOT NULL REFERENCES subnets_tags (uuid)
+        );
+    """
+    __tablename__ = 'subnets'
+    uuid = Column(UUID, primary_key=True, server_default=text("uuid_generate_v4()"))
+    name = Column(Text, nullable=False)
+    tag_uuid = Column(UUID, ForeignKey('subnets_tags.uuid'), nullable=False)
